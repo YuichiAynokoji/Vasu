bbe4ebf0a70ebb78fe9f99062b253b76a339cb0d
YuichiAynokoji <loplor.0908@gmail.com>
2026-03-26 10:29:17 +0700

    добавлен вывод списка артистов

--- music/settings.py
+++ music/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'music.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
