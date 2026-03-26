028b8a5deb508df274308181bde86b5e6570a241
YuichiAynokoji <loplor.0908@gmail.com>
2026-03-26 09:47:23 +0700

    добавлена модель исполнителя

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+# жанр музыки
+class Genre(models.Model):
+    name_en = models.CharField(max_length=500, unique=True)
+    name_ru = models.CharField(max_length=500, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name_ru
+
+# исполнитель (группа или человек)
+class Artist(models.Model):
+    name = models.CharField(max_length=500, unique=True)
+    image = models.ImageField(upload_to='artists/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+# музыкальные треки
+class Track(models.Model):
+    title = models.CharField(max_length=500, unique=True)
+    duration = models.IntegerField(help_text="Длительность в секундах")
+    genres = models.ManyToManyField(Genre)
+    artist = models.ForeignKey(Artist, on_delete=models.RESTRICT, null=True)
+    audio_file = models.FileField(upload_to='music/tracks/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- main/views.py
+++ main/views.py
@@ -1,40 +1,9 @@
 from django.shortcuts import render
+from .models import Genre
 
 def index(request):
     return render(request, 'index.html')
 
 def genres(request):
-    genres_list = [
-        {
-            'id': 1,
-            'name_en': 'Pop',
-            'name_ru': 'Поп',
-            'description': 'Мелодичная музыка с простой структурой, ориентированная на массового слушателя.'
-        },
-        {
-            'id': 2,
-            'name_en': 'Rock',
-            'name_ru': 'Рок',
-            'description': 'Музыка с акцентом на электрогитару, ударные и энергичный ритм.'
-        },
-        {
-            'id': 3,
-            'name_en': 'Hip-Hop',
-            'name_ru': 'Хип-хоп',
-            'description': 'Стиль, основанный на ритмичном речитативе под биты и сэмплы.'
-        },
-        {
-            'id': 4,
-            'name_en': 'Electronic',
-            'name_ru': 'Электронная',
-            'description': 'Музыка, созданная с помощью электронных инструментов и синтезаторов.'
-        },
-        {
-            'id': 5,
-            'name_en': 'Jazz',
-            'name_ru': 'Джаз',
-            'description': 'Музыка, характеризующаяся импровизацией и сложной гармонией.'
-        },
-    ]
-
+    genres_list = Genre.objects.all()
     return render(request, 'genres.html', {'genres': genres_list})
